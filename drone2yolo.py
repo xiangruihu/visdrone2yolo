0bcdcae65b15d583742601bee76d06452ffdfc2e
xiangruiA <69159525+xiangruiA@users.noreply.github.com>
2022-12-12 21:29:28 +0800

    Update drone2yolo.py

--- drone2yolo.py
+++ drone2yolo.py
@@ -1,8 +1,9 @@
 import os
-import cv2
-drone_texts_path = "Annotations"  # 设置Annotations的文件夹路径
-drone_images_path = "Images"      #设置 Images 的文件夹路径
-yolo_txt_path = "yolo_lable"   # 设置转换后的文件保存路径
+from PIL import Image
+
+drone_texts_path = "Annotations"  # 原有 annotations的保存路径
+drone_images_path = "Images"  # 图片的保存路径
+yolo_txt_path = "yolo_lable"  #转换后图片txt的保存路径
 
 drone_txts = os.listdir(drone_texts_path)
 
@@ -18,8 +19,10 @@ def drone2yolo(line):
 # 写入每一个文件 需要对坐标点进行归一化处理 x/w y/h
 def write_yolo(img_path,file,top_left,bot_right,category):
     # os.makedirs("yolo_lable")
-    img = cv2.imread(img_path)
-    h,w,c = img.shape
+    # img = cv2.imread(img_path)
+    # h,w,c = img.shape
+    img = Image.open(img_path)
+    w,h = img.size
     f = open(file,'a')
     f.write(category)
     f.write(" ")
@@ -40,37 +43,21 @@ def write_yolo(img_path,file,top_left,bot_right,category):
 
 
 # 写入文件
-for txt in drone_txts:
-    for line in open(os.path.join("Annotations",txt)).readlines():
-        top_left, bot_right, category = drone2yolo(line)
-        txt_path = os.path.join(yolo_txt_path,txt)
+a = 0
+try:
+    for txt in drone_txts:
+        for line in open(os.path.join(drone_texts_path,txt)).readlines():
+            top_left, bot_right, category = drone2yolo(line)
+            txt_path = os.path.join(yolo_txt_path,txt)
 
-        img_path = os.path.join(drone_images_path,txt[:-3] + "jpg")
-        write_yolo(img_path,txt_path,top_left,bot_right,category)
-
-
-
-
-
-
-# def get_each_file(txtfile_name):
-#         path = os.path.join("Annotations",txtfile_name)
-#         f = open(path,'r')
-#         return f.readlines()
-
-
-
-# if __name__ == '__main__':
-#     # l = get_each_file(labels[0])
-#     # print(l)
+            img_path = os.path.join(drone_images_path,txt[:-3] + "jpg")
+            write_yolo(img_path,txt_path,top_left,bot_right,category)
+        print("正在写入第{:}个文件".format(a))
+        a+=1
+except:
+    print("error")
 
 
 
 
-# 函数2 获取每一个txt文件的信息：
 
-# for i in labels:
-#     label_path = os.path.join("Annotations",i)
-#     f = open(label_path,"r")
-#     for target in f.readlines():
-#         print(target)
